e7ca58d7eb250bba2b046e9bd14f56f0da6eb475
coin0527 <nako2020@naver.com>
2024-01-25 08:35:16 +0900

    .

--- src/Home/Archiving.js
+++ src/Home/Archiving.js
@@ -8,10 +8,69 @@ const Wrap = styled.div`
   background-color: beige;
 `;
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+const Box = styled.div`
+  margin-top: 30px;
+  width: 600px;
+  height: 300px;
+  border: 1px solid #eeeeee;
+  border-radius: 20px;
+  margin: 30px auto;
+  cursor: pointer;
+  padding: 30px 5%;
+  background-color: #fff;
+
+  h2 {
+    font-size: 30px;
+    font-weight: 600;
+  }
+  h3 {
+    margin: 25px 0 25px 0;
+  }
+  p {
+    margin-bottom: 15px;
+    font-size: 20px;
+  }
+
+  &:hover {
+    border: 1px solid #453a33;
+    transition: 0.5s;
+  }
+`;
+
+const Line = styled.div`
+  border: 1px solid black;
+  margin-top: 10px;
+`;
+
 export const Archiving = () => {
   return (
     <Wrap>
       <Title titlename="Archiving" />
+      <Container>
+        <Box>
+          <h2> GitHub </h2>
+          <Line />
+          <h3> https://github.com/coin0527 </h3>
+          <p> ✦ 과거 프로젝트, 프로그램, App의 소스코드 </p>
+          <p> ✦ 혼자서 코딩 연습을 위해 끄적이던 소스 코드 </p>
+          <p> ✦ 학부 시절 수강한 수업들의 코딩 과제 소스 코드 </p>
+        </Box>
+        <Box>
+          <h2> Notion </h2>
+          <Line />
+          <h3>
+            https://www.notion.so/Movie_project-69f5ff9570e441459913601d303305a1
+          </h3>
+          <p> ✦ 과거 프로젝트, 프로그램, App의 과정 </p>
+          <p> ✦ 구체적인 계획, 일정표 </p>
+          <p> ✦ 기획의도 및 프로젝트를 진행하며 느낀 점 </p>
+        </Box>
+      </Container>
     </Wrap>
   );
 };
