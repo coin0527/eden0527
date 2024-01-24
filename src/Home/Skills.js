0e1ea79a063fcb4ebc9c814629dc0a37d33ec51a
coin0527 <nako2020@naver.com>
2024-01-24 18:55:16 +0900

    .

--- src/Home/Skills.js
+++ src/Home/Skills.js
@@ -2,22 +2,33 @@ import styled from "styled-components";
 import { Title } from "../components/Title";
 import { Skilltitle } from "../components/Skilltitle";
 
+import frontendImage from "../Img/frontend.PNG";
+import versionControlImage from "../Img/versioncontrol.PNG";
+
 const Wrap = styled.div`
   width: 100%;
-  height: 100vh;
+  height: 90vh;
   padding: 30px 5%;
 `;
 
 const Box = styled.div`
   width: 100%;
-  max-width: 50%;
+  max-width: 300px;
+  height: 60vh;
   border: 1px solid #453a33;
   border-radius: 10px;
+  background: url(${(props) => props.backgroundImage}) center;
+  background-repeat: no-repeat;
+  transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(0.95);
+  }
 `;
 
 const Container = styled.div`
   display: flex;
-  flex-direction: column;
+  justify-content: center;
   margin-top: 100px;
 `;
 
@@ -26,16 +37,15 @@ export const Skills = () => {
     <Wrap>
       <Title titlename="Skills" />
       <Container>
-        <Box>
+        <Box backgroundImage={frontendImage} style={{ marginRight: "10px" }}>
           <Skilltitle title={"Frontend"} />
         </Box>
 
-        <Box>
+        <Box
+          backgroundImage={versionControlImage}
+          style={{ marginLeft: "10px" }}
+        >
           <Skilltitle title={"Version Control"} />
-        </Box>
-
-        <Box>
-          <Skilltitle title={"Mobile App"} />
         </Box>
       </Container>
     </Wrap>
