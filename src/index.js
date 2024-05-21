1c411cd08ff50e888ee32dbd79c9e856c00bc3bc
coin0527 <nako2020@naver.com>
2024-05-21 21:50:45 +0900

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { Router } from "./Router";
 import { GlobalStyled } from "./css/GlobalStyled";
 
-// 리모델링
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
